01e22005ed8d6466edf60366517bffc0f8a8aad7
relativehysteria <rruziro@gmail.com>
2021-10-10 09:15:32 +0200

    Added formatted duration text

--- src/song.py
+++ src/song.py
@@ -3,16 +3,35 @@ class Song():
         try:
             # If we encounter an error during indexing into the result,
             # this song is simply rendered invalid..
-            self.valid    = True
-            self.uploader = ytdl_result["uploader"]
-            self.url      = ytdl_result["webpage_url"]
-            self.duration = ytdl_result["duration"]
-            self.stream   = get_stream_url(ytdl_result)
+            self.valid              = True
+            self.uploader           = ytdl_result["uploader"]
+            self.url                = ytdl_result["webpage_url"]
+            self.duration           = ytdl_result["duration"]
+            self.stream             = get_stream_url(ytdl_result)
+            self.duration_formatted = parse_duration(self.duration)
         except:
-            self.valid    = False
+            self.valid              = False
 
 def get_stream_url(ytdl_result: dict):
     """Returns the first found audio stream"""
     for i in ytdl_result['formats']:
         if i['acodec'] != "none":
             return i['url']
+
+
+def parse_duration(duration: int):
+    minutes, seconds = divmod(duration, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+
+    duration = []
+    if days > 0:
+        duration.append('{} days'.format(days))
+    if hours > 0:
+        duration.append('{} hours'.format(hours))
+    if minutes > 0:
+        duration.append('{} minutes'.format(minutes))
+    if seconds > 0:
+        duration.append('{} seconds'.format(seconds))
+
+    return ', '.join(duration)
