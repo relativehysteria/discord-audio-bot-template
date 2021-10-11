aa9d8fc6bdc74186d1e278d1342a42af266bc377
relativehysteria <rruziro@gmail.com>
2021-10-12 00:12:06 +0200

    Created the primary logger

---
+++ src/settings.py
@@ -0,0 +1,11 @@
+import logging
+
+## Settings for the primary logger
+
+LOG_FILE        = r"naga.log"
+LOG_LEVEL       = logging.DEBUG
+LOG_FORMAT      = \
+    "{asctime} | {levelname:^8} | {name} | ({filename}:{lineno}) >> {message}"
+LOG_TIME_FORMAT = "%H:%M:%S"
+USE_LOG_COLORS  = True   # Whether to use colored output in the terminal
+LOG_TO_FILE     = True   # Whether to log into a file as well
