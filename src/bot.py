relativehysteria/discord-audio-bot-template

commit 7ae2686f00d39c87710e5b5acc34b88808f3ca1a

One XXX down, also a better fix than 35846c2

src/bot.py
@@ -53,8 +53,9 @@ class Naga(commands.Cog):
     async def _leave_vc(self, ctx: commands.Context):
         """Leaves a voice channel"""
         if ctx.queue.voice:
+            await self.voice.disconnect()
+            await ctx.queue.destruct()
             del self.queues[ctx.guild.id]
-            await ctx.queue.voice.disconnect()
             await ctx.message.add_reaction(REACTION_OK)
 
 
@@ -83,8 +84,10 @@ class Naga(commands.Cog):
         #   1. Something like "https://www.youtube.com/watch?v=fKKNPLowteY&list=PL8VoWXtCcI7jg259j9_kmze0WhqUIkBEM&index=1"
         #      downloads the whole playlist ._.
         #   2. 403
-        #   3. Stop downloading when the bot disconnects
         for url in urls:
+            # Stop downloading when the bot disconnects
+            if ctx.queue._stop_thread:
+                break
             song = Song(url)
 
             # Create an embed and send it to the server if the song has a title,

src/songqueue.py
@@ -62,10 +62,12 @@ class SongQueue():
         return len(self.songs.queue)
 
 
-    def __del__(self):
+    def destruct(self):
         self._stop_thread = True
         self.clear()
+        gLog.debug("Waiting for the queue thread to join...")
         self._thread.join()
+        gLog.debug("Queue thread joined!")
 
 
     def clear(self):

commit 4a184f64c4809f83b986f8a605a84eccc458703d

Fixed a bug introduced in 7ae2686

src/bot.py
@@ -53,7 +53,7 @@ class Naga(commands.Cog):
     async def _leave_vc(self, ctx: commands.Context):
         """Leaves a voice channel"""
         if ctx.queue.voice:
-            await self.voice.disconnect()
+            await ctx.queue.voice.disconnect()
             await ctx.queue.destruct()
             del self.queues[ctx.guild.id]
             await ctx.message.add_reaction(REACTION_OK)