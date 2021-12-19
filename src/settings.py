e973e8eb92b3273ff40789e5b6c4b4bdffde0112
relativehysteria <rruziro@gmail.com>
2021-12-19 14:44:33 +0100

    Changed datefmt in log.py

--- src/settings.py
+++ src/settings.py
@@ -7,7 +7,7 @@ LOG_FILE        = r"naga.log"
 LOG_LEVEL       = logging.INFO
 LOG_FORMAT      = \
     "{asctime} | {levelname:^8} | {name} | ({filename}:{lineno}) >> {message}"
-LOG_TIME_FORMAT = "%H:%M:%S"
+LOG_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 USE_LOG_COLORS  = True   # Whether to use colored output in the terminal
 LOG_TO_FILE     = True   # Whether to log into a file as well
 
