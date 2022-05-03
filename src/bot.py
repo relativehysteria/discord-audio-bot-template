b245fece443d4cdb90d867810b58da0cfc9dde30
relativehysteria <rruziro@gmail.com>
2022-05-03 03:29:31 +0200

    Command testing or something like that
    i fucking hate discord holy fucking shit this thing is annoying as fuck
    you can't even imagine how painful this is

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from log import globalLog as gLog
 
 class Naga(discord.Client):
@@ -11,9 +12,25 @@ class Naga(discord.Client):
 
         # Command tree (for slash commands)
         self.cmd_tree = discord.app_commands.CommandTree(self)
+        self.cmd_tree.add_command(self.test_cmd)
+
+
+    async def setup_hook(self):
+        # TODO:
+        #   Global commands take up to one hour to sync :^)
+        #   Maybe copy the commands to every server the bot is connected to
+        #   so that we don't have to wait so long...
+        gLog.debug(f"Synchronizing commands with Discord...")
+        await self.cmd_tree.sync()
 
 
     async def on_ready(self):
         latency = int(self.latency * 1000)
         gLog.info("READY!")
         gLog.info(f"Startup latency: {latency}ms")
+
+
+    @app_commands.command(name="test", description="test command .-.")
+    async def test_cmd(self, interaction: discord.Interaction):
+        gLog.debug("Called! :D")
+        await interaction.response.send_message("test test test")
